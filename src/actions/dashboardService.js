e4c52a737327bed8b055cb2ce00941a71634fe46
ElvinEgs <proega3@gmail.com>
2019-11-16 13:24:16 +0300

    return axios data

--- src/actions/dashboardService.js
+++ src/actions/dashboardService.js
@@ -1,16 +1,18 @@
 import axiosConfig from '../config/axios';
+import {toastr} from "react-redux-toastr";
 
 export const dashboardService = {
     getDashboard,
 };
 
 function getDashboard() {
-    axiosConfig
+   return axiosConfig
         .get('/issue_tracking/dashboard')
         .then(response => {
-            // return  response;
+             return  response;
         })
         .catch((error) => {
-            //return  error;
+            toastr.error('Error', 'Failed to fetch');
+            return  error;
         });
-};
+}
