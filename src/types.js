84be3ad3c4c348c841886c6b0b70c4d4b5aa9b05
ElvinEgs <proega3@gmail.com>
2019-11-16 10:40:24 +0300

    add type dahsboard

--- src/types.js
+++ src/types.js
@@ -1,3 +1,4 @@
 export const LOGIN_USER = 'LOGIN_USER';
 export const LOGIN_ERROR = 'LOGIN_ERROR';
 export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const DASHBOARD_REQUEST = 'DASHBOARD_REQUEST';
