c9d12c605181609972906f3a953a32db855efa2a
ElvinEgs <proega3@gmail.com>
2019-11-16 10:40:46 +0300

    update dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import NavBar from './dashboard/NavBar'
+import SideBar from './dashboard/SideBar'
+import Home from './dashboard/Home'
+import Footer from './dashboard/Footer'
+import {dashboardService} from "../actions/dashboardService";
 
-const Dashboard = () => {
-    return <div>Dashboard</div>
+class Dashboard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            pager: {},
+            pageOfItems: []
+        };
+    }
+
+    componentDidMount() {
+        this.loadPage();
+
+    }
+
+    componentDidUpdate() {
+        this.loadPage();
+    }
+
+    loadPage() {
+        dashboardService.getDashboard();
+    }
+
+    render() {
+        const {pager, pageOfItems} = this.state;
+        return (<div id="app">
+                <NavBar/>
+                <SideBar/>
+                <Home/>
+                <Footer/>
+            </div>
+        );
+    };
 }
-
 export default Dashboard;
